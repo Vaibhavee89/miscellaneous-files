f893360ae5d2011b657b0efc88bab3da600937a3
Vaibhavee Singh <vaibhaveesingh89@gmail.com>
2023-11-24 22:25:31 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,5 @@
+#write a function that calculates the average of the lists of numbers
+
+def average(numbers):
+    return sum(numbers)/ len(numbers)
+print (average([1, 2,3, 4, 5, 6, 7, 8, 9,10]))#There are more such tools like github copilot and codex
