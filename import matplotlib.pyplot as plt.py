880c1d2871bbe6721a381926a10041cb0badb2f0
Vaibhavee Singh <vaibhaveesingh89@gmail.com>
2023-11-24 22:06:02 +0530

    Add files via upload

---
+++ import matplotlib.pyplot as plt.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+x=[1,2,3,4,5,6,7,8,9]
+y=[1,3,5,1,3,5,3,1]
+plt.legend(x,y,label="line L")
+plt.xlabel('x axis')
+plt.ylabel('y axis')
+plt.title('Line Graph Example')
+plt.legend()
+plt.show()
